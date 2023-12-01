afaaeaed4dec7029e55350a56cbc9e8ca18cd530
Kappi Patterson <kappi.patterson@gmail.com>
2023-12-02 00:11:15 +0100

    Adding comments

--- visual_odometry/continuous_vo/associate_keypoints_to_existing_landmarks.py
+++ visual_odometry/continuous_vo/associate_keypoints_to_existing_landmarks.py
@@ -43,21 +43,23 @@ def track_and_update(
     }
 
     # Track the keypoints in the new image, note we transpose the images.
+    # new_keypoints.shape = Nx1x2
     new_keypoints, status, _ = cv2.calcOpticalFlowPyrLK(
         prev_image.T, new_image.T, prev_keypoints, None, **lk_params
     )
 
     # Filter out the keypoints for which tracking was successful
-    tracked_new = new_keypoints[status == 1]
-    tracked_prev = prev_keypoints[status == 1]
-    tracked_3D = state.X[:, status.flatten().astype(bool)]
+    tracked_new = new_keypoints[status == 1]  # Shape: Nx2
+    tracked_prev = prev_keypoints[status == 1]  # Shape: Nx2
+    tracked_3D = state.X[:, status.flatten().astype(bool)]  # Shape: 3xN
 
     if visualize:
+        # Squeeze the arrays to get rid of the added dimension.
         visualize_keypoint_movement_with_status(
             "Keypoint Movement with Status",
             visualizer_img,
-            prev_keypoints,
-            new_keypoints,
+            prev_keypoints.squeeze(),
+            new_keypoints.squeeze(),
             status,
         )
 
@@ -66,21 +68,23 @@ def track_and_update(
     # TODO: Check this is the intended way they want us to use RANSAC and that we are
     # allowed to use this opencv function.
     _, inlier_mask = cv2.findFundamentalMat(tracked_prev, tracked_new, cv2.FM_RANSAC)
-    inlier_new = tracked_new.reshape(-1, 1, 2)[inlier_mask == 1]
-    inlier_prev = tracked_prev.reshape(-1, 1, 2)[inlier_mask == 1]
-    inlier_3D = tracked_3D[:, inlier_mask.flatten().astype(bool)]
+    # ravel is a broadcasting thing to make inlier_mask 1d so it can be used across each
+    # row of the arrays it's indexing.
+    inlier_new = tracked_new[inlier_mask.ravel() == 1]  # Shape Nx2
+    inlier_prev = tracked_prev[inlier_mask.ravel() == 1]  # Shape Nx2
+    inlier_3D = tracked_3D[:, inlier_mask.flatten().astype(bool)]  # Shape 3xN
 
     if visualize:
         visualize_keypoint_movement_with_status(
             "After running ransac",
             visualizer_img,
-            inlier_prev.reshape(-1, 1, 2),
-            inlier_new.reshape(-1, 1, 2),
+            inlier_prev,
+            inlier_new,
             np.ones((inlier_new.shape[0], 1)),
         )
 
     # Update keypoints in the state
-    # Transform back to 2xN format
+    # Transform P back to 2xN format
     state.update_landmarks(inlier_3D, inlier_new.T)
 
 
@@ -98,8 +102,8 @@ def visualize_keypoint_movement_with_status(
     Parameters:
         title (str): The title to show on the plot.
         new_image (np.ndarray): The new image frame where keypoints are to be visualized.
-        prev_keypoints (np.ndarray): The keypoints in the previous image, in the shape returned by OpenCV: (Mx1x2)
-        new_keypoints (np.ndarray): The keypoints in the new image, in the shape returned by OpenCV: (Mx1x2)
+        prev_keypoints (np.ndarray): The keypoints in the previous image, in the shape returned by OpenCV: (Mx2)
+        new_keypoints (np.ndarray): The keypoints in the new image, in the shape returned by OpenCV: (Mx2)
         status (np.ndarray): Array indicating whether each keypoint was successfully tracked, shape (Mx1)
 
     Returns:
@@ -109,9 +113,9 @@ def visualize_keypoint_movement_with_status(
 
     # Iterate through keypoints and status
     for p1, p2, s in zip(prev_keypoints, new_keypoints, status):
-        # Convert to integer tuples and conver to x,y (swap axes)
-        p1 = (int(p1[0][1]), int(p1[0][0]))
-        p2 = (int(p2[0][1]), int(p2[0][0]))
+        # Convert to integer tuples and convert to x,y (swap axes)
+        p1 = (int(p1[1]), int(p1[0]))
+        p2 = (int(p2[1]), int(p2[0]))
 
         if s[0] == 1:  # Successfully tracked
             cv2.circle(
