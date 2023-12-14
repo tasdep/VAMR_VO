851a12e001db0d91f8b4f5e6283f2bf53ca332a8
Ádám Weinhardt <wkadam9@gmail.com>
2023-12-14 10:16:26 +0100

    f

--- visual_odometry/bootstrapping/initialization.py
+++ visual_odometry/bootstrapping/initialization.py
@@ -6,6 +6,11 @@ import matplotlib.pyplot as plt
 import params.params as params
 from utils.visualisation import drawCamera
 from utils.state import State
+from utils.image_processing import (
+    run_harris_detector,
+    patch_describe_keypoints,
+    triangulate_points_wrapper,
+)
 from utils.utils import create_homogeneous_matrix
 
 
@@ -15,94 +20,75 @@ def initialize_pipeline(
     K: np.ndarray,
     visualise: bool = False,
     print_stats: bool = False,
+    prematached_keypoints: list[np.ndarray] = None,
 ) -> State:
     # 2 frames from dataset and params
+    # images are [rows, columns] so need to be indexed [y,x]
     img_1: np.ndarray = input_images[params.BOOTSRAP_FRAMES[0], ...]
     img_2: np.ndarray = input_images[params.BOOTSRAP_FRAMES[1], ...]
 
     assert img_1.shape == img_2.shape
-
-    # find keypoint correspondences between frames, option to use intermediate frames
-    harris_params = {
-        "blockSize": params.HARRIS_BLOCK_SIZE,
-        "ksize": params.HARRIS_SOBEL_SIZE,
-        "k": params.HARRIS_K,
-    }
-    corners_1: np.ndarray = cv2.cornerHarris(img_1, **harris_params)
-    corners_2: np.ndarray = cv2.cornerHarris(img_2, **harris_params)
-
-    # extract keypoints from corner detector
-    keypoints_1 = np.argwhere(corners_1 > params.KEYPOINT_THRESHOLD * corners_1.max())
-    keypoints_2 = np.argwhere(corners_2 > params.KEYPOINT_THRESHOLD * corners_2.max())
-
     if print_stats:
-        print(f"{keypoints_1.shape=}")
-        print(f"{keypoints_2.shape=}")
+        print(f"{img_1.shape=}")
 
-    if visualise:
-        fig, axs = plt.subplots(1, 2)
-        axs[0].imshow(img_1, cmap="gray")
-        axs[0].plot(keypoints_1[:, 1], keypoints_1[:, 0], "rx")
-        axs[1].imshow(img_2, cmap="gray")
-        axs[1].plot(keypoints_2[:, 1], keypoints_2[:, 0], "rx")
-        plt.show()
+    if prematached_keypoints is None:
+        # normal workflow of detecting keypoints and matching between images
+        keypoints_1 = run_harris_detector(img_1, visualise, print_stats)
+        keypoints_2 = run_harris_detector(img_2, visualise, print_stats)
 
-    # TODO try adaptive thresholding, https://docs.opencv.org/3.4/d7/d4d/tutorial_py_thresholding.html to acheive more uniform keypoints
-    # TODO non maxima suppresion to select best n keypoints or just to reduce any crossover
-    # TODO try KLT instead of descriptor matching
+        # TODO try adaptive thresholding, https://docs.opencv.org/3.4/d7/d4d/tutorial_py_thresholding.html to acheive more uniform keypoints
+        # TODO non maxima suppresion to select best n keypoints or just to reduce any crossover
+        # TODO try KLT instead of descriptor matching
 
-    # calculate patch descriptors
-    descriptors_1: np.ndarray = describe_keypoints(
-        img_1, keypoints_1, params.DESC_PATCH_RAD
-    )
-    descriptors_2: np.ndarray = describe_keypoints(
-        img_2, keypoints_2, params.DESC_PATCH_RAD
-    )
+        # calculate patch descriptors
+        descriptors_1: np.ndarray = patch_describe_keypoints(img_1, keypoints_1, params.DESC_PATCH_RAD)
+        descriptors_2: np.ndarray = patch_describe_keypoints(img_2, keypoints_2, params.DESC_PATCH_RAD)
 
-    # TODO try ratio test instead of crossCheck
+        # TODO try ratio test instead of crossCheck
 
-    # match the descriptors between images
-    # cross check only returns matches where the nearest keypoint is consistent between images
-    bf: cv2.BFMatcher = cv2.BFMatcher.create(cv2.NORM_L2, crossCheck=True)
-    matches = bf.match(
-        queryDescriptors=descriptors_1.astype(np.float32),
-        trainDescriptors=descriptors_2.astype(np.float32),
-    )
-
-    if print_stats:
-        print(f"{len(matches)=}")
-
-    # Create keypoint objects from numpy arrays
-    keypoints1_lst: list[cv2.KeyPoint] = [
-        cv2.KeyPoint(float(x), float(y), 1) for y, x in keypoints_1
-    ]
-    keypoints2_lst: list[cv2.KeyPoint] = [
-        cv2.KeyPoint(float(x), float(y), 1) for y, x in keypoints_2
-    ]
-
-    if visualise:
-        img3 = cv2.drawMatches(
-            img_1,
-            keypoints1_lst,
-            img_2,
-            keypoints2_lst,
-            matches,
-            None,
-            flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
+        # match the descriptors between images
+        # cross check only returns matches where the nearest keypoint is consistent between images
+        bf: cv2.BFMatcher = cv2.BFMatcher.create(cv2.NORM_L2, crossCheck=True)
+        matches = bf.match(
+            queryDescriptors=descriptors_1.astype(np.float32),
+            trainDescriptors=descriptors_2.astype(np.float32),
         )
-        plt.imshow(img3)
-        plt.show()
 
-    # get matched keypoints to estimate fundamental matrix
-    matched_pts_1 = np.float32([keypoints_1[m.queryIdx] for m in matches]).reshape(
-        -1, 2
-    )
-    matched_pts_2 = np.float32([keypoints_2[m.trainIdx] for m in matches]).reshape(
-        -1, 2
-    )
+        if print_stats:
+            print(f"{len(matches)=}")
+
+        # Create keypoint objects from numpy arrays
+        keypoints1_lst: list[cv2.KeyPoint] = [cv2.KeyPoint(float(x), float(y), 1) for x, y in keypoints_1]
+        keypoints2_lst: list[cv2.KeyPoint] = [cv2.KeyPoint(float(x), float(y), 1) for x, y in keypoints_2]
+
+        if visualise:
+            img3 = cv2.drawMatches(
+                img_1,
+                keypoints1_lst,
+                img_2,
+                keypoints2_lst,
+                matches,
+                None,
+                flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
+            )
+            plt.imshow(img3)
+            plt.show()
+
+        # get matched keypoints to estimate fundamental matrix
+        matched_pts_1 = np.float32([keypoints_1[m.queryIdx] for m in matches]).reshape(-1, 2)
+        matched_pts_2 = np.float32([keypoints_2[m.trainIdx] for m in matches]).reshape(-1, 2)
+    else:
+        # load in the matched points
+        matched_pts_1 = np.float32(prematached_keypoints[0])
+        matched_pts_2 = np.float32(prematached_keypoints[1])
+        print(f"{matched_pts_1.shape=}")
+        print(f"{matched_pts_2.shape=}")
 
     fundamental: np.ndarray
     mask: np.ndarray
+
+    # CODE FROM HERE REQUIRES (X,Y) TO FUNCTION CORRECTLY BUT DOES WORK
+    # SAME RESULTS AS FROM EX6
     fundamental, mask = cv2.findFundamentalMat(
         matched_pts_1,
         matched_pts_2,
@@ -132,7 +118,7 @@ def initialize_pipeline(
     # find essential matrix
     essential: np.ndarray = K.T @ fundamental @ K
 
-    # get remaining keypoints after RANSAC
+    # get remaining keypoints after RANSAC during fundamental matrix
     inlier_pts_1: np.ndarray = matched_pts_1[mask.ravel() == 1]
     inlier_pts_2: np.ndarray = matched_pts_2[mask.ravel() == 1]
     if print_stats:
@@ -152,48 +138,25 @@ def initialize_pipeline(
         R2[:, 0] *= -1
 
     # find correct combination out of the 4 possible options
-    R_correct, t_correct = disambiguateRelativePose(
-        [R1, R2], t, inlier_pts_1, inlier_pts_2, K
-    )
-
+    R_correct, t_correct = disambiguateRelativePose([R1, R2], t, inlier_pts_1, inlier_pts_2, K)
+    T: np.ndarray = create_homogeneous_matrix(R_correct, t_correct)
     # 3D
-    X: np.ndarray = triangulate_points_wrapper(
-        R_correct, t_correct, K, inlier_pts_1, inlier_pts_2
-    )
+    X, mask = triangulate_points_wrapper(np.eye(4), T, K, inlier_pts_1.T, inlier_pts_2.T)
+    if X[2, :].min() < 0:
+        n = (X[2,:]<0).sum()
+        print(f"{n} points triangulated behind camera during initialisation")
     # 2D
-    P: np.ndarray = inlier_pts_2.T
+    P: np.ndarray = inlier_pts_2.T[:, mask]
 
     # Visualisation
     if visualise:
-        visualise_pose_and_landmarks(
-            R_correct, t_correct, img_1, img_2, X, inlier_pts_1, inlier_pts_2
-        )
+        visualise_pose_and_landmarks(R_correct, t_correct, img_1, img_2, X, inlier_pts_1, inlier_pts_2)
 
     # initialise the pipeline object with the keypoints and landmarks
     # note: C,F,T are blank as there are no candidates at this point
     state = State()
     state.update_landmarks(X, P)
-    return state, create_homogeneous_matrix(R_correct, t_correct)
-
-
-def describe_keypoints(img: np.ndarray, keypoints: np.ndarray, r: int) -> np.ndarray:
-    """
-    Returns a (2r+1)^2xN matrix of image patch vectors based on image img and a 2xN matrix containing the keypoint
-    coordinates. r is the patch "radius".
-    """
-    N: int = keypoints.shape[0]
-    descriptors: np.ndarray = np.zeros([N, (2 * r + 1) ** 2])
-    padded: np.ndarray = np.pad(
-        img, [(r, r), (r, r)], mode="constant", constant_values=0
-    )
-
-    for i in range(N):
-        kp: np.ndarray = keypoints[i, :].astype(int) + r
-        descriptors[i, :] = padded[
-            (kp[0] - r) : (kp[0] + r + 1), (kp[1] - r) : (kp[1] + r + 1)
-        ].flatten()
-
-    return descriptors
+    return state, T
 
 
 def disambiguateRelativePose(
@@ -232,11 +195,14 @@ def disambiguateRelativePose(
             M2: np.ndarray = K @ np.c_[rot, t]
             P_C1: np.ndarray = cv2.triangulatePoints(M1, M2, points_1.T, points_2.T)
 
-            # project in both cameras
+            # dehomegnise
+            P_C1: np.ndarray = P_C1 / P_C1[3, :]
+
+            # Transform 3D points to camera 2's frame of reference.
+            # not sure this is correct, I don't think we want a K dependence here
             P_C2: np.ndarray = np.c_[rot, t] @ P_C1
 
-            # dehomegnise
-            P_C1: np.ndarray = P_C1[0:3, :] / P_C1[3, :]
+            P_C1 = P_C1[0:3, :]
 
             num_points_in_front1: int = np.sum(P_C1[2, :] > 0)
             num_points_in_front2: int = np.sum(P_C2[2, :] > 0)
@@ -250,21 +216,6 @@ def disambiguateRelativePose(
                 total_points_in_front_best = total_points_in_front
 
     return R, T
-
-
-def triangulate_points_wrapper(
-    rot: np.ndarray,
-    t: np.ndarray,
-    K: np.ndarray,
-    points_1: np.ndarray,
-    points_2: np.ndarray,
-):
-    M1: np.ndarray = K @ np.eye(3, 4)
-    M2: np.ndarray = K @ np.c_[rot, t]
-    points_3D: np.ndarray = cv2.triangulatePoints(M1, M2, points_1.T, points_2.T)
-    # dehomgenize
-    points_3D: np.ndarray = points_3D[0:3, :] / points_3D[3, :]
-    return points_3D
 
 
 def visualise_pose_and_landmarks(
@@ -285,15 +236,13 @@ def visualise_pose_and_landmarks(
     # homogeneous coordinates), given by the function linearTriangulation
     ax.scatter(X[0, :], X[1, :], X[2, :], marker="o")
     # Display camera pose
-    drawCamera(ax, np.zeros((3,)), np.eye(3), length_scale=2)
+    drawCamera(ax, np.zeros((3,)), np.eye(3), length_scale=10)
     ax.text(-0.1, -0.1, -0.1, "Cam 1")
 
     center_cam2_W = -R.T @ t
     center_cam2_W = center_cam2_W.reshape(-1)
-    drawCamera(ax, center_cam2_W, R.T, length_scale=2)
-    ax.text(
-        center_cam2_W[0] - 0.1, center_cam2_W[1] - 0.1, center_cam2_W[2] - 0.1, "Cam 2"
-    )
+    drawCamera(ax, center_cam2_W, R.T, length_scale=10)
+    ax.text(center_cam2_W[0] - 0.1, center_cam2_W[1] - 0.1, center_cam2_W[2] - 0.1, "Cam 2")
 
     # ax.set_xlim([-5, 50])
     # ax.set_ylim([-5, 50])
@@ -302,12 +251,12 @@ def visualise_pose_and_landmarks(
     # Display matched points
     ax = fig.add_subplot(1, 3, 2)
     ax.imshow(img_1)
-    ax.scatter(inliers_1.T[1, :], inliers_1.T[0, :], color="y", marker="s")
+    ax.scatter(inliers_1.T[0, :], inliers_1.T[1, :], color="y", marker="s")
     ax.set_title("Image 1")
 
     ax = fig.add_subplot(1, 3, 3)
     ax.imshow(img_2)
-    ax.scatter(inliers_2.T[1, :], inliers_2.T[0, :], color="y", marker="s")
+    ax.scatter(inliers_2.T[0, :], inliers_2.T[1, :], color="y", marker="s")
     ax.set_title("Image 2")
 
     plt.show()
---
+++ visual_odometry/utils/image_processing.py
@@ -0,0 +1,149 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import params.params as params
+
+
+def run_harris_detector(img: np.ndarray, visualise: bool = False, print_stats: bool = False):
+    """
+    Detects keypoints using the Harris corner detector on the given image.
+
+    Parameters:
+    - img: The input grayscale image.
+    - visualise: Flag to visualize the detected keypoints.
+    - print_stats: Flag to print statistics about the detected keypoints.
+
+    Returns:
+    - A Nx2 array containing N keypoint coordinates (column, row) or (x,y) where corners are detected.
+    """
+    detector_params = {
+        "maxCorners": 500,
+        "qualityLevel": 0.01,
+        "minDistance": 10,
+        "blockSize": 3,
+    }
+
+    # cv2.goodfeaturestotrackwithquality also returns the quality scores of the detected corners
+    keypoints = cv2.goodFeaturesToTrack(img, **detector_params)
+
+    keypoints = keypoints.reshape((-1, 2))
+
+    if print_stats:
+        print(f"{keypoints.shape=}")
+
+    if visualise:
+        fig, axs = plt.subplots(1, 1)
+        axs.imshow(img, cmap="gray")
+        axs.plot(keypoints[:, 0], keypoints[:, 1], "rx")
+        plt.show()
+
+    return keypoints
+
+
+def patch_describe_keypoints(img: np.ndarray, keypoints: np.ndarray, r: int) -> np.ndarray:
+    """
+    Extracts image patch descriptors centered around keypoints.
+
+    Parameters:
+    - img: The input image.
+    - keypoints: A Nx2 matrix containing N keypoint coordinates (column, row).
+    - r: The patch "radius." The size of each square patch is (2r+1)x(2r+1).
+
+    Returns:
+    - A Nx(2r+1)^2 matrix of image patch vectors. Each row corresponds to a keypoint,
+      and the patch vector is a flattened (2r+1)x(2r+1) square region centered around the keypoint.
+      If keypoints is None, an empty matrix with shape (0, (2r+1)^2) is returned.
+    """
+    if keypoints is None:
+        return np.ndarray((0, (2 * r + 1) ** 2))
+    N: int = keypoints.shape[0]
+    descriptors: np.ndarray = np.zeros([N, (2 * r + 1) ** 2])
+    padded: np.ndarray = np.pad(img, [(r, r), (r, r)], mode="constant", constant_values=0)
+
+    for i in range(N):
+        kp: np.ndarray = keypoints[i, :].astype(int) + r
+        descriptors[i, :] = padded[(kp[1] - r) : (kp[1] + r + 1), (kp[0] - r) : (kp[0] + r + 1)].flatten()
+
+    return descriptors
+
+
+def triangulate_points_wrapper(T1: np.ndarray, T2: np.ndarray, K: np.ndarray, points_1: np.ndarray, points_2: np.ndarray)-> (np.ndarray, np.ndarray):
+    """
+    Triangulates 3D points from corresponding 2D points in two camera views.
+
+    Parameters:
+    - T1: The transformation matrix (4x4) of the first camera.
+    - T2: The transformation matrix (4x4) of the second camera.
+    - K: The camera intrinsic matrix.
+    - points_1: 2xN array of 2D pixel coordinates in the first image.
+    - points_2: 2xN array of corresponding 2D pixel coordinates in the second image.
+
+    Returns:
+    - A 3xN array containing the triangulated 3D coordinates of the points, IN THE WORLD FRAME.
+    - A boolean mask array (N) corresponding to the filtered points, False entry if point removed
+    """
+    # dehomogenize transforms
+    T1 = T1[0:3, :]
+    T2 = T2[0:3, :]
+    M1: np.ndarray = K @ T1
+    M2: np.ndarray = K @ T2
+    # points in world frame
+    points_3D: np.ndarray = cv2.triangulatePoints(M1, M2, points_1, points_2)
+
+    # dehomogenise to form x,y,z,1
+    points_3D: np.ndarray = points_3D / points_3D[3, :]
+
+    # get points in different camera frames
+    P_C1: np.ndarray = T1 @ points_3D
+    P_C2: np.ndarray = T2 @ points_3D
+
+    # filter world points to remove points that are behind either camera
+    mask: np.ndarray = np.logical_and(P_C1[2, :] > 0, P_C2[2, :] > 0)
+    num_points_behind_camera: int = mask.size - np.sum(mask)
+    # print(f"{num_points_behind_camera} points filtered from behind camera.")
+    points_3D = points_3D[0:3, mask]
+    # dehomgenize
+    return points_3D, mask
+
+
+# IDEA FOR NON LINEAR REPROJECTION OPTIMISATION
+import numpy as np
+from scipy.optimize import least_squares
+
+# Assuming you have the following variables from your previous code
+# K1, K2: Intrinsic matrices for cameras 1 and 2
+# dist_coeffs1, dist_coeffs2: Distortion coefficients for cameras 1 and 2
+# rvec1, tvec1, rvec2, tvec2: Rotation and translation vectors for cameras 1 and 2
+# point1, point2: Corresponding 2D points in images 1 and 2
+# initial_point: Initial estimate of the 3D point
+
+
+# Function to compute reprojection error
+def reprojection_error(params, K, dist_coeffs, rvec, tvec, point_2d):
+    x, y, z = params
+    point_3d = np.array([x, y, z])
+
+    # Project the 3D point to 2D using the camera parameters
+    projected_point, _ = cv2.projectPoints(np.array([point_3d]), rvec, tvec, K, dist_coeffs)
+
+    # Compute the reprojection error
+    error = point_2d - projected_point[0, 0]
+
+    return error
+
+
+def test_reprojection_optimisation():
+    # Initial estimate of the 3D point
+    # initial_point = np.array([x, y, z])
+
+    # Bundle adjustment using least_squares
+    result = None
+    # result = least_squares(reprojection_error, initial_point,
+    #                     args=(K1, dist_coeffs1, rvec1, tvec1, point1),
+    #                     method='lm')
+
+    # Refined 3D point coordinates
+    refined_point = result.x
+
+    # Print the refined point
+    print("Refined 3D Point:", refined_point)
